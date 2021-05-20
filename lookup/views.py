2896028c51a79395257e05c642cc6e90bff02d5c
Snigdho <asifrahmansnigdho@gmail.com>
2021-05-20 10:41:30 +0600

    Seventy percent done

--- lookup/views.py
+++ lookup/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
 def home(request):
-	return render(request,'home.html',{})
+	import json
+	import requests
+
+	api_requests= requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=19106&distance=5&API_KEY=0416B2DD-3ABE-4698-9F78-433D7B46FA55")
+
+	try:
+		api = json.loads(api_requests.content)
+	except Exception as e:
+		api = "Error..."
+	
+	return render(request,'home.html',{'api': api})
 
 def about(request):
 	return render(request, 'about.html' ,{})	
